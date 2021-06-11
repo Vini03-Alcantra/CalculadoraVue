6be4238750b5e463b37710b0229245d404d6a72e
Vinicus <viniciusalcantarabarros@gmail.com>
2021-06-10 23:08:23 -0300

    estruturando

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <h1>Hello wolrd</h1>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    export default{
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    body{
+        margin: 0;
+    }
+
+    #app{
+        display: flex;
+        flex-direction: column;
+        height: 100vh;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+
+        color: #fff;
+        background: linear-gradient(to right, rgb(83, 105, 118), rgb(41, 46, 73));
+    }
 </style>
